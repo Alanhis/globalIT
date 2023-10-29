dbb93090e4ce2d9e4171e21386808dfdbf3713b0
Alan Khismatullin <alanhis043@gmail.com>
2023-10-29 14:21:24 +0300

    Добавление поиска

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,24 +2,37 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './component/Card/Card'
 import Modal from './component/Modal/Modal';
-
+import SearchSVG from './assets/search.svg'
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [users, setUsers] = useState()
   const [data, setData] = useState()
+  const [filterUsers, setFilterUsers] = useState(users)
   useEffect(() => {
     async function getData(){
      await fetch("http://127.0.0.1:3000/")
      .then(data => data.json())
-     .then(res => setUsers(res))
+     .then(res => {setUsers(res);setFilterUsers(res)})
     }
     getData()
   }, [])
   return (
-    <main style={{display: "grid", gridTemplateColumns: "400px auto 40%"}}>
-      {users && users.map((user, index) => <Card key={index} data={user} setData={setData} setIsOpen={setIsOpen}/>)}
-      {isOpen && <Modal data={data} setIsOpen={setIsOpen} />}
-    </main>
+    <><header>
+      <div className='header_container'>
+        <input className='header_input' onChange={(e) => {
+         if( e.target.value.trim() !== ''){
+          setFilterUsers(users.filter(sortData => sortData.name.includes(e.target.value.trim())))
+         } else{
+          setFilterUsers(users)
+         }
+        }}>
+        </input>
+        <img src={SearchSVG}/>
+      </div>
+    </header><main className='main_container'>
+        {filterUsers && filterUsers.map((user, index) => <Card key={index} data={user} setData={setData} setIsOpen={setIsOpen} />)}
+        {isOpen && <Modal data={data} setIsOpen={setIsOpen} />}
+      </main></>
   )
 }
 
--- client/src/component/Card/Card.jsx
+++ client/src/component/Card/Card.jsx
@@ -1,15 +1,17 @@
-
-
+import PhoneSVG from '../../assets/phone.svg'
+import EmailSVG from '../../assets/email.svg'
+import styles from "./Card.module.css"
 function Card({data, setData, setIsOpen}) {
-console.log(data)
   return (
-    <div onClick={() => {setIsOpen(true); setData(data)}}>
+    <div className={styles.card_container} onClick={() => {setIsOpen(true); setData(data)}}>
       <h2>{data.name}</h2>
-      <div>
-        <p>{data.phone}</p>
+      <div className={styles.card_text_container}>
+        <img src={PhoneSVG} />
+        <p className={styles.cart_text}>{data.phone}</p>
       </div>
-      <div>
-        <p>{data.email}</p>
+      <div className={styles.card_text_container}>
+        <img src={EmailSVG} />
+        <p className={styles.cart_text}>{data.email}</p>
       </div>
     </div>
   )
